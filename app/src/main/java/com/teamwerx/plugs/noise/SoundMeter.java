0b415ae66c74c008e0b23fa7acf2b8e37932f422
Kevin D. Wolf <kevinw@software-logistics.com>
2019-09-25 14:28:01 -0400

    WIP - smart audio capture

--- app/src/main/java/com/teamwerx/plugs/noise/SoundMeter.java
+++ app/src/main/java/com/teamwerx/plugs/noise/SoundMeter.java
@@ -24,11 +24,40 @@ import com.teamwerx.plugs.Services.UploadHelper;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
 public class SoundMeter {
     static final private double EMA_FILTER = 0.6;
+
+    public enum DetectorState {
+        /* Not detecting */
+        Idle,
+
+        /* Waiting for threshold to be exceeded. */
+        Detecting,
+
+        /* A level higher then the audio threshold has been seen, continue to record audio */
+        Recording,
+
+        /* Wait for a low level for a certain amount of time, before terminating the recording */
+        PendingCompletion
+    }
+
+    /*
+            v---------------------------|
+        Detecting => Recording => PendingCompletion
+                          ^-------------|
+     */
+
+    private DetectorState mDetectorState = DetectorState.Detecting.Idle;
+    private long mTimeInStateMS = 0;
+    private long mRecordingStarted = 0;
+
+    private LocalDateTime mStartTimeInState;
+    private LocalDateTime mRecordingStart;
 
     private String mCurrentFileName;
     private String mDeviceId;
@@ -41,16 +70,42 @@ public class SoundMeter {
 
     private String mServer;
 
-    public SoundMeter() {
+    private DetectorSettings mDetectorSettings;
+
+    public class DetectorSettings {
+        /* Audio level to be met to start recording */
+        public double ThreadholdLevel;
+
+        /*  Maximum amount of time no sound will be recorded prior to the file being recycled.  */
+        public long LoopTime;
+
+        /* Maximum amount of time audio is recorded before it will be uploaded to the server */
+        public long MaxAudioTimeMS;
+
+        /* Number of milliseconds that have passed where threshold is not met. */
+        public long SilencePeriodMS;
 
     }
 
+    public SoundMeter() {
+        File file = new File(Environment.getExternalStorageDirectory() + "/Plugs");
+        if(!file.isDirectory())
+        {
+            file.mkdir();
+        }
 
-    private String getFileName() {
+        this.mDetectorSettings = new DetectorSettings();
+        this.mDetectorSettings.LoopTime = 5000;
+        this.mDetectorSettings.MaxAudioTimeMS = 15000;
+        this.mDetectorSettings.ThreadholdLevel = 1.0;
+        this.mDetectorSettings.SilencePeriodMS = 5000;
+    }
+
+    private String getFullTimeStampFileName() {
         String deviceId = mDeviceId == null || mDeviceId.length() == 0 ? "????" : mDeviceId;
         Calendar current = Calendar.getInstance();
 
-        return String.format("%s_%04d%02d%02d%02d%02d%02d",
+        String fileName = String.format("%s_%04d%02d%02d%02d%02d%02d",
                 deviceId,
                 current.get(Calendar.YEAR),
                 current.get(Calendar.MONTH),
@@ -58,6 +113,8 @@ public class SoundMeter {
                 current.get(Calendar.HOUR_OF_DAY),
                 current.get(Calendar.MINUTE),
                 current.get(Calendar.SECOND));
+
+        return Environment.getExternalStorageDirectory().getAbsolutePath() + "/Plugs/" + fileName + ".3gp";
     }
 
     public void start() {
@@ -68,14 +125,14 @@ public class SoundMeter {
             mMediaDetector.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
             mMediaDetector.setOutputFile("/dev/null");
 
-            mMediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
+            /*mMediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
                 @Override
                 public void onInfo(MediaRecorder mr, int what, int extra) {
                     if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED) {
                       //  mMediaRecorder.setNextOutputFile("/dev/null");
                     }
                 }
-            });
+            });*/
 
             try {
                 mMediaDetector.prepare();
@@ -97,32 +154,15 @@ public class SoundMeter {
         this.mPort = port;
     }
 
-    public void startRecording() {
-        Date date = new Date();
-
-        mMediaDetector.stop();
-        mMediaDetector = null;
-
-        File file = new File(Environment.getExternalStorageDirectory() + "/Plugs");
-        if(!file.isDirectory())
-        {
-            file.mkdir();
-        }
-
-        String fileName = getFileName();
-
-        mCurrentFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Plugs/" + fileName + ".3gp";
-
-        mMediaRecorder = new MediaRecorder();
-        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mMediaRecorder.setOutputFile(mCurrentFileName);
-
-        Log.d("PLUGSAPP", mCurrentFileName);
+    MediaRecorder startMediaRecorder(String fileName) {
+        MediaRecorder  mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFile(fileName);
 
         try {
-            mMediaRecorder.prepare();
+            mediaRecorder.prepare();
         }
         catch (IllegalStateException e) {
             e.printStackTrace();
@@ -130,18 +170,67 @@ public class SoundMeter {
         catch (IOException e) {
             e.printStackTrace();
         }
+
+        return mediaRecorder;
+    }
+
+    public void update() {
+        long millis = ChronoUnit.MILLIS.between(mRecordingStart, LocalDateTime.now());
+
+        switch(mDetectorState) {
+            case Detecting:
+                Calendar current = Calendar.getInstance();
+
+                if(mMediaRecorder.getMaxAmplitude() > this.mDetectorSettings.ThreadholdLevel) {
+                    this.mDetectorState = DetectorState.Recording;
+                }
+                else if(millis > this.mDetectorSettings.LoopTime) {
+                    mMediaRecorder.stop();
+                    mCurrentFileName = getFullTimeStampFileName();
+                    mMediaRecorder = startMediaRecorder((mCurrentFileName));
+                }
+
+                break;
+            case Recording:
+                if(millis > this.mDetectorSettings.MaxAudioTimeMS) {
+                    mMediaRecorder.stop();
+                    String oldFileName = mCurrentFileName;
+                    mCurrentFileName = getFullTimeStampFileName();
+                    mMediaRecorder = startMediaRecorder((mCurrentFileName));
+                    uploadFile(oldFileName);
+                }
+
+                break;
+            case PendingCompletion:
+                break;
+                default:
+                    break;
+        }
+    }
+
+    public void startRecording() {
+        mMediaDetector.stop();
+        mMediaDetector = null;
+
+        mCurrentFileName = getFullTimeStampFileName();
+
+        mMediaRecorder = startMediaRecorder(mCurrentFileName);
+
         mMediaRecorder.start();
+    }
+
+    private void uploadFile(String fileName) {
+        if(mDeviceId != null && mDeviceId.length() > 0 && mServerName != null && mServerName.length() > 0) {
+            UploadHelper uploader = new UploadHelper(mDeviceId, this.mServerName, this.mPort);
+            uploader.setMedia(fileName, "audio/3gpp");
+            uploader.execute();
+        }
     }
 
     public void stopRecording() {
         mMediaRecorder.stop();
         mMediaRecorder = null;
 
-        if(mDeviceId != null && mDeviceId.length() > 0 && mServerName != null && mServerName.length() > 0) {
-            UploadHelper uploader = new UploadHelper(mDeviceId, this.mServerName, this.mPort);
-            uploader.setMedia(mCurrentFileName, "audio/3gpp");
-            uploader.execute();
-        }
 
         mCurrentFileName = null;
 
